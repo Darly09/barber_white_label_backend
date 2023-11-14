f2eaff1f134d61e97a825beb6ee2f6f20435c0e8
JuanP0329 <jpbenjumeago@cesde.net>
2023-11-13 19:07:19 -0500

    Product endpoint router added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { LoginRoutes } = require("./View/LoginView.js");
+const { ProductsRoutes } = require("./View/ProductView.js");
 
 const PORT = 3000;
 const app = express();
@@ -8,7 +9,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use("/api", LoginRoutes);
+app.use("/api", LoginRoutes, ProductsRoutes);
 
 app.listen(PORT);
-console.log(`Servidor Corriendo en el puerto ${PORT}`)
+console.log(`Servidor Corriendo en el puerto ${PORT}`);
