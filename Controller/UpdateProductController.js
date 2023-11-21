e5bf1a2979e3042d2c2fc71f495be873d73e1d3a
JuanP0329 <jpbenjumeago@cesde.net>
2023-11-21 18:50:45 -0500

    endpoint business logic is created to update a product.

---
+++ Controller/UpdateProductController.js
@@ -0,0 +1,22 @@
+const { selectProduct } = require("../Data/ProductsData.js");
+
+async function updateProduct(product, reference) {
+  const products = await selectProduct();
+  const productIndex = products.findIndex(
+    (product) => product.reference == reference
+  );
+
+  if (
+    productIndex === -1 ||
+    productIndex === null ||
+    productIndex === undefined
+  ) {
+    throw Error("Producto no encontrado.");
+  }
+
+  products[productIndex] = product;
+
+  return products[productIndex];
+}
+
+module.exports = { updateProduct };
