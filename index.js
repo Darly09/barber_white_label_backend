e8b5553cbead34543352ed8ab7a18470858c35ea
JuanP0329 <jpbenjumeago@cesde.net>
2023-11-21 19:29:43 -0500

    the path of the endpoint to modify products is added to the port configuration.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const { LoginRoutes } = require("./View/LoginView.js");
 const { ProductsRoutes } = require("./View/ProductView.js");
 const { ProductsByRefRoutes } = require("./View/ProductByRefView.js");
+const { ProductUpdateRoutes } = require("./View/UpdateProductView.js");
 
 const PORT = 3000;
 const app = express();
@@ -10,7 +11,13 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use("/api", LoginRoutes, ProductsRoutes, ProductsByRefRoutes);
+app.use(
+  "/api",
+  LoginRoutes,
+  ProductsRoutes,
+  ProductsByRefRoutes,
+  ProductUpdateRoutes
+);
 
 app.listen(PORT);
 console.log(`Servidor Corriendo en el puerto ${PORT}`);
