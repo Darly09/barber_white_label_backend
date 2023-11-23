1c2317283eb3b8068b815d1803c92bfe67d38b84
sebas <jhoan-071995@hotmail.com>
2023-11-22 19:41:30 -0500

    A folder called data is created containing a ServicesData.js file that contains the data and will make the connection to supabase

---
+++ data/ServicesData.js
@@ -0,0 +1,34 @@
+const { Service } = require("../model/Service");
+
+async function selectServices() {
+  const servicesMock = [
+    new Service(
+      1,
+      "Corte",
+      1,
+      20,
+      "Servicio de corte de pelo",
+      "url_de_la_imagen",
+      1
+    ),
+
+    new Service(
+      2,
+      "Barba",
+      2,
+      15.0,
+      "Servicio de arreglo de barba",
+      "url_de_la_imagen",
+      1
+    ),
+  ];
+
+  return servicesMock;
+}
+
+// //se creas const servicesMock para poder add el servicio 
+const servicesMock = [
+  new Service(1, "Corte", 1, 20, "Servicio de corte de pelo", "url_de_la_imagen", 1),
+  new Service(2, "Barba", 2, 15.0, "Servicio de arreglo de barba", "url_de_la_imagen", 1),
+];
+module.exports = { selectServices, servicesMock };
