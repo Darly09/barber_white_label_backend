ee8422faff939625665bd87d3e6c40c18d93d4aa
sebas <jhoan-071995@hotmail.com>
2023-11-22 21:50:14 -0500

    code is cleared from the login controller file

--- Controller/LoginController.js
+++ Controller/LoginController.js
@@ -1,40 +1,11 @@
+async function login(login) {
+  const emailMock = "backend@gmail.com";
+  const passwordMock = "123LaNea4**";
 
-const emailMock = "backend@gmail.com";
-const passwordMock = "123LaNea4**";
+  const isCorrectEmail = emailMock === login.email;
+  const isCorrectPassword = passwordMock === login.password;
 
-
-async function login(email, password) {
-  const isCorrectEmail = emailMock === email;
-  const isCorrectPassword = passwordMock === password;
-
-  return isCorrectEmail && isCorrectPassword
+  return isCorrectEmail && isCorrectPassword;
 }
 
 module.exports = { login };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// async function login(login) {
-//   const emailMock = "backend@gmail.com";
-//   const passwordMock = "123LaNea4**";
-
-//   const isCorrectEmail = emailMock === login.email;
-//   const isCorrectPassword = passwordMock === login.password;
-
-//   return isCorrectEmail && isCorrectPassword;
-// }
-
-// module.exports = { login };
