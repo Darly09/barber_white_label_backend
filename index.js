7ebe8836d9584828ac7fb70b295aee2ef49cb30c
sebas <jhoan-071995@hotmail.com>
2023-11-22 19:51:58 -0500

    in the index.js the routers are added to add service and a response container is created for the endpoint of addServiceController.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { LoginRoutes } = require("./view/LoginView.js");
-const { ServiRoutes } = require("./view/ServiView.js")
+const { ServiceRoutes } = require("./view/ServiceView.js")
+const { login } = require("./controller/LoginController.js");
+const { addService } = require("./controller/addServiceController.js");
+
 
 const PORT = 3000;
 const app = express();
@@ -9,7 +12,20 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use("/api", LoginRoutes, ServiRoutes);
+app.use("/api", LoginRoutes, ServiceRoutes);
+
+
+  app.post("/api/services", (req, res) => {
+    try {
+     
+      const newServiceData = req.body;
+      const newService = addService(newServiceData);
+  
+      res.status(201).json(newService); 
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  });
 
 app.listen(PORT);
 console.log(`Servidor Corriendo en el puerto ${PORT}`)
