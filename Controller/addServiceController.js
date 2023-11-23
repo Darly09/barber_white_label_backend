5880dfad6cb3ec6b2ed0f1f5d71701c8cca6e8d9
sebas <jhoan-071995@hotmail.com>
2023-11-22 19:22:30 -0500

    A file is created in the controller folder called addServiceController.js which has the endpoint for creating new services

---
+++ Controller/addServiceController.js
@@ -0,0 +1,39 @@
+const { servicesMock } = require("../data/ServicesData.js");
+
+
+function addService(newServiceData) {
+  
+  if (!newServiceData.name || !newServiceData.price) {
+    throw new Error("Nombre y precio son campos obligatorios");
+  }
+
+  const newService = {
+    id: servicesMock.length + 1,
+    ...newServiceData,
+  };
+
+  servicesMock.push(newService);
+
+  return newService;
+}
+
+module.exports = { addService };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
