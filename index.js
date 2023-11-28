09504f63b6e542f3088673fa433f5c94350aa647
JuanP0329 <jpbenjumeago@cesde.net>
2023-11-27 23:02:34 -0500

    the endpoint view update services is modified and added to the port to be consumed with the client.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { LoginRoutes } = require("./view/LoginView.js");
-const { ServiceRoutes } = require("./view/ServiceView.js")
-const { ProductsRoutes } = require("./View/ProductView.js");
-const { ProductsByRefRoutes } = require("./View/ProductByRefView.js");
-const { ProductUpdateRoutes } = require("./View/UpdateProductView.js");
-
+const { ServiceRoutes } = require("./view/ServiceView.js");
+const { ProductsRoutes } = require("./view/ProductView.js");
+const { ProductsByRefRoutes } = require("./view/ProductByRefView.js");
+const { ProductUpdateRoutes } = require("./view/UpdateProductView.js");
+const { ServiceUpdateRoutes } = require("./view/UpdateServiceView.js");
 
 const PORT = 3000;
 const app = express();
@@ -13,14 +13,14 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
 app.use(
   "/api",
   LoginRoutes,
   ProductsRoutes,
   ServiceRoutes,
   ProductsByRefRoutes,
-  ProductUpdateRoutes
+  ProductUpdateRoutes,
+  ServiceUpdateRoutes
 );
 
 app.listen(PORT);
