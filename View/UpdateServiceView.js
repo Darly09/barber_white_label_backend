0223e880d67496a3d22c492e5b1f43f66cb58e81
JuanP0329 <jpbenjumeago@cesde.net>
2023-11-27 22:56:37 -0500

    the view is created with the path and its presentation logic for the endpoint to update the services.

---
+++ View/UpdateServiceView.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const { updateService } = require("../controller/UpdateServiceController.js");
+const ServiceUpdateRoutes = express.Router();
+
+ServiceUpdateRoutes.put("/updateService/:id", async (req, res) => {
+  let statusCode = 200;
+  try {
+    const id = req.params.id;
+    const service = req.body;
+    const update = await updateService(service, id);
+    res.status(statusCode);
+    res.send({
+      code: statusCode,
+      message: "El Servicio se actualizó correctamente.",
+      data: update,
+    });
+  } catch (error) {
+    statusCode = 404;
+    res.status(statusCode);
+    res.send({
+      code: statusCode,
+      message: error.message,
+    });
+  }
+});
+
+module.export = { ServiceUpdateRoutes };
