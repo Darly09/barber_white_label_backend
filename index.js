6e6a6c2156c9054a9ab4b8b63fe3f984706146ec
JSBHA <jhoan-071995@hotmail.com>
2023-11-29 19:17:17 -0500

    the delete path is added

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import { ProductsByRefRoutes } from "./view/ProductByRefView.js";
 import { ProductUpdateRoutes } from "./view/UpdateProductView.js";
 import { ServiceUpdateRoutes } from "./view/UpdateServiceView.js";
 import { AddProductRoutes } from "./view/AddProductView.js";
+const { DeleteService } = require("./controller/serviceController.js");
 
 const PORT = 3000;
 const app = express();
@@ -22,7 +23,8 @@ app.use(
   ProductsByRefRoutes,
   ProductUpdateRoutes,
   ServiceUpdateRoutes,
-  AddProductRoutes
+  AddProductRoutes,
+  DeleteService,
 );
 
 app.listen(PORT);
